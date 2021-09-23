f2bf79a04fcecf9bb588ddba3a098f7f58ca4cd7
Harshsanas <harrysanas140@gmail.com>
2021-09-23 18:42:12 +0530

    backend src file setup

---
+++ Backend/src/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+
+//starting nodemon server
+const start = async () => {
+  app.listen(3033, () => {
+    console.log("listening on port 3033");
+  });
+};
+
+start();

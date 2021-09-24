01beec0c5a8c5f03ffea3dc201a8dd3a050555df
Harshsanas <harrysanas140@gmail.com>
2021-09-25 01:17:18 +0530

    backend setup

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,10 +4,19 @@ const app = express();
 
 const cors = require("cors");
 
+app.use(express.json()); // usefull while posting and updating;
+
+const connect = require("./config/db");
+
+const formController = require("./controller/form.controller");
+
+app.use(cors());
+
+app.use("/", formController);
 
 //starting nodemon server
 const start = async () => {
-
+  await connect();
   app.listen(3033, () => {
     console.log("listening on port 3033");
   });
